377257962db047e57267b23b33fbda83360191f7
Haeeb Ahmad <xxxeebi@gmail.com>
2018-04-21 02:49:47 +0500

    Send Email to User Email Address During Registration

--- api/controllers/RegisterController.js
+++ api/controllers/RegisterController.js
@@ -4,7 +4,7 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+var nodemailer = require('nodemailer');
 module.exports = {
   signup: function(req,res) {
     var userParams = {
@@ -17,6 +17,35 @@ module.exports = {
     User.create(userParams).fetch().exec(function(err, user) {
       if (err)
         res.badRequest(err);
+      //=================================================================================================
+      //Email Send to User
+
+        var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'nexthrmofficial@gmail.com',
+            pass: 'haseeb898'
+          },
+          tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false
+          }
+        });
+        const mailOptions = {
+          from: 'nexthrmofficial@gmail.com', // sender address
+          to: user.email, // list of receivers
+          subject: 'NextHrm Email', // Subject line
+          html: '<p>Welcome </p>' + user.first_name + 'To Next Hrm'// plain text body
+        };
+        transporter.sendMail(mailOptions, function (err, info) {
+          if(err)
+            console.log(err)
+          else
+            console.log(info);
+        });
+
+      // End of user email
+      //=================================================================================================
       var params = {
         name: req.body.company.name,
         owner: user.id
